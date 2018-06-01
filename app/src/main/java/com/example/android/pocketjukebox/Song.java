00dc77f7025c38a153a98c9111b1db785812a466
Adam <datriphopper@gmail.com>
2018-05-31 21:01:37 -0500

    Added song Java class

---
+++ app/src/main/java/com/example/android/pocketjukebox/Song.java
@@ -0,0 +1,23 @@
+package com.example.android.pocketjukebox;
+
+public class Song {
+    //String value
+    private String mArtist;
+    //String value
+    private String mTitle;
+
+    //Constructor method takes two String parameters
+    public Song(String artist, String title) {
+        mArtist = artist;
+        mTitle = title;
+    }
+
+    //accessor aka "getter" methods
+    public String getArtist(){
+        return mArtist;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+}
