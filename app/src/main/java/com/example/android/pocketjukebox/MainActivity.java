1f0a4d7410712de67b2ef2911e220315599468cc
Adam <datriphopper@gmail.com>
2018-06-04 18:07:57 -0500

    Added intent and OnClickListener to access Available songs list

--- app/src/main/java/com/example/android/pocketjukebox/MainActivity.java
+++ app/src/main/java/com/example/android/pocketjukebox/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.pocketjukebox;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //find button add songs
+        Button addSong = (Button) findViewById(R.id.add_button);
+
+        //set onClick listener with intent to navigate to list of available songs
+        addSong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent addIntent = new Intent(MainActivity.this, AvailableSongsActivity.class);
+                //start the new activity
+                startActivity(addIntent);
+            }
+        });
+
+
     }
 }
